ac4802144ce08020514e8533298ece3660d29ec2
Ethan Kuo <ethankuocodes@gmail.com>
2026-06-19 10:58:31 -0700

    rewrite expected_returns.py

--- expected_returns.py
+++ expected_returns.py
@@ -1,5 +1,4 @@
 import yfinance as yf
-import seaborn as sns
 import numpy as np
 
 tickers = ['UPRO', 'SPY', 'SPXU', 'GOOG', 'ASTS']
@@ -7,13 +6,15 @@ prices = yf.download(tickers, start='2024-01-01', auto_adjust=True)['Close']
 returns = prices.pct_change(fill_method=None).dropna()
 
 weights = np.array([.2, .2, .2, .2, .2])
-cov_matrix = returns.cov().values * 252 
-portfolio_variance = np.dot(weights.T, np.dot(cov_matrix, weights))
-portfolio_volatility = np.sqrt(portfolio_variance)
 
-avg_daily_returns = returns.mean()
-annualized_returns = avg_daily_returns * 252
-portfolio_expected_return = np.dot(weights, annualized_returns)
+asset_growth = prices / prices.iloc[0]
+portfolio_value = asset_growth @ weights
+portfolio_returns = portfolio_value.pct_change().dropna()
 
+years = len(portfolio_returns) / 252
+portfolio_expected_return = portfolio_value.iloc[-1] ** (1 / years) - 1
+
+portfolio_volatility = portfolio_returns.std() * np.sqrt(252)
+
+print(f"Expected CAGR: {portfolio_expected_return}")
 print(f"Portfolio Volatility: {portfolio_volatility}")
-print(f"Expected Returns: {portfolio_expected_return}")
